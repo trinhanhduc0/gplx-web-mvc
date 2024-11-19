06ad9ef2b3d067cfa4c1748f8a181098ded812dc
trinhanhduc0 <trinhanhduc7078@gmail.com>
2024-11-20 06:18:04 +0700

    Add docker file

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+﻿# Base image cho runtime
+FROM mcr.microsoft.com/dotnet/aspnet:6.0 AS base
+WORKDIR /app
+EXPOSE 80
+
+# Base image cho build
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY . .
+RUN dotnet restore
+RUN dotnet publish -c Release -o /app/out
+
+# Image cuối cùng
+FROM base AS final
+WORKDIR /app
+COPY --from=build /app/out .
+ENTRYPOINT ["dotnet", "GPLX.dll"]
--- obj/Debug/net6.0/GPLX.AssemblyInfo.cs
+++ obj/Debug/net6.0/GPLX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GPLX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd9a76a258f71139ebb68b5a24a0fcfafff729cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GPLX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GPLX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
